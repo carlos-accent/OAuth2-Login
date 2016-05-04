2d1d9b385eed18d19e3e356e68f4ca7447837524
Carlos Gómez Lasala <carlos.gomezlasala@accenture.com>
2016-05-04 15:27:58 +0200

    removed all tags of a server configuration

--- src/main/java/com/example/Oauth2LoginApplication.java
+++ src/main/java/com/example/Oauth2LoginApplication.java
@@ -1,28 +1,15 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableResourceServer
-@RestController
+@EnableOAuth2Sso
 public class Oauth2LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2LoginApplication.class, args);
-	}
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
 	}
 
 }
